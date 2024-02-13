7d4b73ab44a4fb6d3e9d7d02e9b5ae981efd89ec
aftlah <altaf.fattah05@gmail.com>
2024-02-13 07:45:10 +0700

    13/02/2024

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+// function evenOrOdd(number) {
+//     if (number % 2 == 0) {
+//         return "Even";
+//     }
+//     return "Odd";
+// }
+
+const evenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd";
+
+    console.log(evenOrOdd(2));
