7526c7fcb0d8232df8baa3fc7565840d8aeb99ea
aftlah <altaf.fattah05@gmail.com>
2024-02-13 07:49:09 +0700

    13/02/2024

---
+++ reverseWords.js
@@ -0,0 +1,6 @@
+// function reverseWords(str) {
+//     return str.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+// }
+
+const reverseWords = (str) => str.split(" ").map(word => word.split('').reverse().join('')).join(" ");
+console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
